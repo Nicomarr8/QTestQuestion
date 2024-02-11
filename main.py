00e3c65dfc17f8234c046fccf4cc5e5af9cdcead
Nicomarr8 <126726313+Nicomarr8@users.noreply.github.com>
2024-02-11 03:32:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Problem Statement: Social media analytics
+
+# Given:
+
+social_media_posts = [
+    {"Username": "user1", "Likes": 100, "Comments": 20, "Shares": 10},
+    {"Username": "user2", "Likes": 50, "Comments": 5, "Shares": 2},
+    {"Username": "user3", "Likes": 200, "Comments": 15, "Shares": 5}
+]
+
+# Expected:
+# Your script should generate a social media analytics report like this:
+
+# Username Likes Comments Shares Engagement
+# user3 200 15 5 220
+# user1 100 20 10 130
+# user2 50 5 2 57
+
+# Total Engagement: 407
