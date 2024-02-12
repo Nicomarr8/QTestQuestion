33708c431193638ae6d992dd51a78a1f28696c35
Nicomarr8 <126726313+Nicomarr8@users.noreply.github.com>
2024-02-12 02:26:47 -0500

    added sorting to the output and handled edgecase of empty input array
    added sorting so the array is sorted greatest to least in terms of engagement, also added some code to handle the array being empty

--- main.py
+++ main.py
@@ -10,12 +10,34 @@ social_media_posts = [
 
 # I made it a function so that I can call it multiple times for testing
 def SocialMediaReport(reportArray):
-    # This variable store the total amount of engagement per array of users
+    # Some code to handle the list being size zero
+    if len(reportArray) < 1:
+        print("Array of Users is too small to create a report on (size of zero)")
+        return
+    # This variable stores the index offset from the end of the list
+    posOffset = -1
+    # The code continues until the offset is pushed to be the beginning of the list
+    # This is a selection sort algorithm working to place the smallest item at the end of the list
+    # I know I could have just implimented a normal selection sort but have it look for the greatest number not the least but it's pretty much the same either way
+    while posOffset > -len(reportArray):
+        # The smallest item is declared first as the last element in the unsorted section of the list
+        minvalElement = reportArray[posOffset]
+        # Then all of the elements, up to the end of the unsorted section, are compared to it to find the smallest value
+        for i in range(len(reportArray) + posOffset):
+            if reportArray[i]["Likes"] + reportArray[i]["Shares"] + reportArray[i]["Comments"] < minvalElement["Likes"] + minvalElement["Shares"] + minvalElement["Comments"]:
+                minvalElement = reportArray[i]
+        # Then, the smallest value element and the element next to the sorted portion of the list swap places
+        reportArray[reportArray.index(minvalElement)] = reportArray[posOffset]
+        reportArray[posOffset] = minvalElement
+        # Then, the offset is decremented by 1, pushing the end of the unsorted section of the list closer to the front by 1 place
+        posOffset -= 1
+
+    # This variable store the total amount of engagement per list of users
     total = 0
     # This prints out the column labels for each column of the report, then a dividing line before the data entries
     print("| Username   | Likes | Comments | Shares | Engagement |")
     print("-------------------------------------------------------")
-    # This for loop loops through every entry in the array of users and prints them out in the format of the report.
+    # This for loop loops through every entry in the list of users and prints them out in the format of the report.
     for i in reportArray:
         # The print statement formats the string so that it looks like a nice and neat grid, the numebrs were chosen arbitrarily or based on teh column names' length, as I don't
         # have any description of overall restrictions such as username length or maximum values for shares, likes, or comments.
@@ -26,21 +48,23 @@ def SocialMediaReport(reportArray):
     print("-------------------------------------------------------")
     print(f"Total Engagement: {total}\n")
 
-# This is the function call for a report to be generated for the original array
+# This is the function call for a report to be generated for the original list
 SocialMediaReport(social_media_posts)
 
-# This is a secondary array containing larger numbers, longer usernames, and more values
+# This is a secondary list containing larger numbers, longer usernames, and more values
 social_media_posts2 = [
     {"Username": "user1", "Likes": 100, "Comments": 20, "Shares": 10},
     {"Username": "user2", "Likes": 50, "Comments": 5, "Shares": 2},
     {"Username": "user3", "Likes": 200, "Comments": 15, "Shares": 5},
     {"Username": "user49", "Likes": 300, "Comments": 7, "Shares": 0},
+    {"Username": "user50", "Likes": 300, "Comments": 7, "Shares": 0},
+    {"Username": "user51", "Likes": 300, "Comments": 7, "Shares": 0},
     {"Username": "user5", "Likes": 200, "Comments": 15, "Shares": 7},
     {"Username": "user621", "Likes": 240, "Comments": 5, "Shares": 9},
     {"Username": "user7000", "Likes": 120, "Comments": 13, "Shares": 1},
 ]
 
-# This is the function call for the test array
+# This is the function call for the test list
 SocialMediaReport(social_media_posts2)
 
 # Expected:
